d6e6bd57b5d6295eb40785598e7118c3f755cada
GilsonRodriguesAlmeida <gilson.almeida@al.infnet.edu.br>
2023-03-11 11:16:21 -0300

    Criado SellersController

---
+++ VendasWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendasWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
